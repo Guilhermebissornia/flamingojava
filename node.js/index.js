d06623de350caed40e550da75f2f7500e9749bd0
Guilhermebissornia <111927456+Guilhermebissornia@users.noreply.github.com>
2023-10-01 11:25:44 -0300

    Create index.js

---
+++ node.js/index.js
@@ -0,0 +1,9 @@
+var x = 1.5;
+var xpto = 0 + (20 * 90 * 75 - 4 + x * ( 20 / 90 /75 + 4 + x * x) ** (20 / 90 / 75 + 4 + x * x) * (2 + 8 * (20 - 1) * - 1)* -1) + 1;
+
+
+
+console.log(" bissornia");
+console.info("bissornia");
+console.error(" bissornia");
+console.warn(xpto);
